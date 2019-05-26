f6ee0ef7ffe9df9be38ae921f61912ae938ef5ed
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-05-26 14:52:53 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -4,6 +4,9 @@ class EventProcessor
       "type": "text",
       #{}"text": "hello,#{text}"+"史密打"
       "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你', '我我').tr('我', '你')
+      "type": "image",
+	  "originalContentUrl": "https://upload.wikimedia.org/wikipedia/commons/9/97/Cat_%282188027041%29.jpg",
+	  "previewImageUrl": "https://upload.wikimedia.org/wikipedia/commons/9/97/Cat_%282188027041%29.jpg"
     }
     return message
   end
