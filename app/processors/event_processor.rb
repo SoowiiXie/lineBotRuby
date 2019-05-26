16d53d1dff724a6b77e907861b6a4f1494a2c38c
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-05-26 14:47:20 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -3,7 +3,7 @@ class EventProcessor
     message = {
       "type": "text",
       #{}"text": "hello,#{text}"+"史密打"
-      "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你', '我我')
+      "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你', '我我').tr('他她', '我覺得')
     }
     return message
   end
