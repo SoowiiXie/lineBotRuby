74b0b5865bc53f158790ae4966ebbe203b32c18d
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-06-23 22:42:15 +0800

    update event processor

--- example/echo.rb
+++ example/echo.rb
@@ -3,7 +3,7 @@ class EventProcessor
     message = {
       "type": "text",
       #{}"text": "hello,#{text}"+"史密打",
-      "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你他', '我我他').tr('我', '你')
+      "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你', '我我').tr('我', '你')
 
     }
     return message
