780037bbd1211e02bf88a2f80a510c845f3a6000
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-06-23 22:54:48 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -1,11 +1,19 @@
-class EventProcessor
+ class EventProcessor
   def process(text)
-    message = {
-      "type": "text",
-      #{}"text": "hello,#{text}"+"史密打",
-      "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你', '我我').tr('我', '你')
+    call_sheety_api.each do |data|
+      if data["keyword"].to_s == text
+        return {  
+          "type": "text",
+          "text": data["message"].to_s
+        }
+      end
+    end
+    return nil
+  end
 
-    }
-    return message
+  def call_sheety_api
+    uri = URI("https://api.sheety.co/6110fffb-76e4-4d8f-9ca1-b27e2c31da26")
+    body = Net::HTTP.get(uri)
+    JSON.parse(body)
   end
 end
