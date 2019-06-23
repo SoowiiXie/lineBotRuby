913ea7c12701227bc652fbcd1e32351967f62b18
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-06-23 22:57:16 +0800

    update event processor

--- example/fake_ai.rb
+++ example/fake_ai.rb
@@ -2,7 +2,9 @@ class EventProcessor
   def process(text)
     message = {
       "type": "text",
-      "text": text.tr('嗎', '').tr('?？', '!！')
+      #{}"text": "hello,#{text}"+"史密打",
+      "text": text.tr('嗎', '').tr('?？', '!！').tr('妳你', '我我').tr('我', '你')
+
     }
     return message
   end
