1dfd38256daface56968143d58131b481b8487be
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-06-26 19:19:23 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -23,23 +23,8 @@ class EventProcessor
     return [
       {
         "type": "image",
-        "originalContentUrl": "https://images.unsplash.com/photo-1477764250597-dffe9f601ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        "previewImageUrl": "https://images.unsplash.com/photo-1477764250597-dffe9f601ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-      },
-      {
-        "type": "image",
-        "originalContentUrl": "https://images.unsplash.com/photo-1519900270450-3a6adec39956?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        "previewImageUrl": "https://images.unsplash.com/photo-1519900270450-3a6adec39956?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-      },
-      {
-        "type": "image",
-        "originalContentUrl": "https://images.unsplash.com/photo-1545460580-36c1957cfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80",
-        "previewImageUrl": "https://images.unsplash.com/photo-1545460580-36c1957cfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80"
-      },
-      {
-        "type": "image",
-        "originalContentUrl": "https://images.unsplash.com/photo-1537787009205-dac68f7e2759?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        "previewImageUrl": "https://images.unsplash.com/photo-1537787009205-dac68f7e2759?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        "originalContentUrl": "https://photos.google.com/album/AF1QipNp81FwEUTq7pLmAgUS5A5K_0wy18eUWNE062nP/photo/AF1QipMTLHsSTLqJqBv_GoUZsR12Pmcw6EmeUEdNower",
+        "previewImageUrl": "https://photos.google.com/album/AF1QipNp81FwEUTq7pLmAgUS5A5K_0wy18eUWNE062nP/photo/AF1QipMTLHsSTLqJqBv_GoUZsR12Pmcw6EmeUEdNower"
       },
       {
         "type": "image",
