3762766e57859bdca1ba704fbe9f5f3974a3828f
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-12-09 15:36:11 +0800

    update event processor

--- example/index.rb
+++ example/index.rb
@@ -122,7 +122,7 @@ class EventProcessor
   end
 
   def sheet(text)
-    call_sheety_api.each do |classmate|
+    call_sheety_api["classmate"] do |classmate|
       if classmate["name"].to_s == text
         return {  
           "type": "text",
