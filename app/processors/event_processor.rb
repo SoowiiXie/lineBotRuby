5ac004c7993964f59cb473f429d58807810b936b
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-12-09 19:51:47 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -142,7 +142,7 @@ class EventProcessor
       facebook = classmate["facebook"]
       live = classmate["宿舍住處"]
       if ((classmate["number"].to_s == text) or (classmate["name"].to_s == text))
-        if classmate["number"]>=1101
+        if (classmate["number"]>=1101)
           return {  
           "type": "text",
           "text": "股名：#{name}
