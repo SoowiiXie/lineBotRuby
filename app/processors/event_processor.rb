5361966d7cd595799196cdca6e89274f39a03570
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-12-09 16:48:47 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -135,15 +135,15 @@ class EventProcessor
       if ((classmate["number"].to_s == text) or (classmate["name"].to_s == text))
         return {  
           "type": "text",
-          "text": "姓名:#{name}
-座號:#{number}
-學號:#{stnumber}
-綽號:#{nickname}
-eMail:#{eMail}
-手機:#{phone}
-LINE:#{line}
-Facebook:#{facebook}
-宿舍住處:#{live}
+          "text": "姓名：#{name}
+座號：#{number}
+學號：#{stnumber}
+綽號：#{nickname}
+eMail：#{eMail}
+手機：#{phone}
+LINE：#{line}
+Facebook：#{facebook}
+宿舍住處：#{live}
 "
         }
       end
