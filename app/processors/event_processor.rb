70eb3ba86b7630f18356069ea6494c62fe266ded
Soowii Xie <vinceshieh@yahoo.com.tw>
2019-12-11 15:49:25 +0800

    update event processor

--- app/processors/event_processor.rb
+++ app/processors/event_processor.rb
@@ -312,7 +312,7 @@ Facebook：#{facebook}
   def answer(text)
     message = {
       "type": "text",
-      "text": text.tr('我', '你').tr('嗎', '').tr('?？', '!！').tr('妳', '我').tr('你', '我')
+      "text": text.tr('我你妳', '你我我').tr('嗎', '').tr('?？', '!！')
     }
     return message
   end
